55ce6072f7f6715d93eed9ce8a312397330bce40
SALIPE <felipebuenosouza@uol.com.br>
2025-03-11 17:57:29 +0000

    changing benchmark dataset

--- scripts/cl/benchmark.sh
+++ scripts/cl/benchmark.sh
@@ -6,8 +6,11 @@
 source /home/a61491/.bashrc
 
 PROJECTHOME=/home/a61491/rrm-genomic-extractor
-TRAIN=/home/a61491/datasets/kmers
-TESTDIR=/home/a61491/datasets/test_voc/test
+#TRAIN=/home/a61491/datasets/kmers
+#TESTDIR=/home/a61491/datasets/test_voc/test
+
+TRAIN=/home/a61491/datasets/denv/kmers
+TESTDIR=/home/a61491/datasets/denv/kmers
 
 
 cd $PROJECTHOME && julia --project Main.jl benchmark -w $1 --train-dir $TRAIN --test-dir $TESTDIR
--- scripts/local/benchmark.sh
+++ scripts/local/benchmark.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 PROJECTHOME=~/Desktop/rrm-genomic-extractor
-TESTDIR=~/Desktop/datasets/test_voc/test
-TRAIN=~/Desktop/datasets/tutorial_data/kmers
+#TESTDIR=~/Desktop/datasets/test_voc/test
+#TRAIN=~/Desktop/datasets/tutorial_data/kmers
 
+TESTDIR=~/Desktop/datasets/denv/kmers
+TRAIN=~/Desktop/datasets/denv/kmers
 
 cd $PROJECTHOME && julia --project Main.jl benchmark -w $1 --train-dir $TRAIN --test-dir $TESTDIR
 
